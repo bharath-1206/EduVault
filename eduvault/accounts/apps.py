5b84b4c0b7322c3d4f0e191823af28fefb5e65eb
Mohan Gowda B L <mohanrgbl6629@gmail.com>
2026-03-09 22:26:10 +0530

    admin creation chnaged to accounts/apps

--- eduvault/accounts/apps.py
+++ eduvault/accounts/apps.py
@@ -3,3 +3,18 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
+
+try:
+    User = get_user_model()
+
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="mohan",
+            email="mohanrgbl6629@gmail.com",
+            password="mohang@1307"
+        )
+except OperationalError:
+    pass
