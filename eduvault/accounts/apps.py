483ece1a1b161fe9af8f8b368ec196061021690a
Mohan Gowda B L <mohanrgbl6629@gmail.com>
2026-03-09 22:44:28 +0530

    auto admin deleted

--- eduvault/accounts/apps.py
+++ eduvault/accounts/apps.py
@@ -4,20 +4,3 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
-
-    def ready(self):
-        from django.contrib.auth import get_user_model
-        from django.db.utils import OperationalError, ProgrammingError
-
-        try:
-            User = get_user_model()
-
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser(
-                    username="mohan",
-                    email="mohanrgbl6629@gmail.com",
-                    password="admin123"
-                )
-        except (OperationalError, ProgrammingError):
-            # Database might not be ready during migration
-            pass
