de65ed9bcda1cdf363a2bb8d0bfe4f218ae53766
Mohan Gowda B L <mohanrgbl6629@gmail.com>
2026-03-09 22:34:04 +0530

    admin creation fixec

--- eduvault/accounts/apps.py
+++ eduvault/accounts/apps.py
@@ -2,19 +2,22 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
-    name = 'accounts'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "accounts"
 
-from django.contrib.auth import get_user_model
-from django.db.utils import OperationalError
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        from django.db.utils import OperationalError, ProgrammingError
 
-try:
-    User = get_user_model()
+        try:
+            User = get_user_model()
 
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="mohan",
-            email="mohanrgbl6629@gmail.com",
-            password="mohang@1307"
-        )
-except OperationalError:
-    pass
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="mohan",
+                    email="mohanrgbl6629@gmail.com",
+                    password="admin123"
+                )
+        except (OperationalError, ProgrammingError):
+            # Database might not be ready during migration
+            pass
