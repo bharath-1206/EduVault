04fd0ef916daf33b6b2ddb2649d2959b8083dbc6
Mohan Gowda B L <mohanrgbl6629@gmail.com>
2026-03-09 22:16:45 +0530

    auto admin created

--- eduvault/eduvault/settings.py
+++ eduvault/eduvault/settings.py
@@ -167,5 +167,18 @@ cloudinary.config(
     api_secret=os.getenv("API_SECRET"),
 )
 
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
 
+try:
+    User = get_user_model()
+
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="mohan",
+            email="mohanrgbl6629@gmail.com",
+            password="mohang@1307"
+        )
+except OperationalError:
+    pass
 
