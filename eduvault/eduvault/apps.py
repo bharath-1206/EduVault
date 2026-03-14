18f6eda5370bcdfa225289c98b0768b4350a40d8
Mohan Gowda B L <mohanrgbl6629@gmail.com>
2026-03-14 11:30:09 +0530

    fix deployment

---
+++ eduvault/eduvault/apps.py
@@ -0,0 +1,21 @@
+from django.apps import AppConfig
+
+
+class EduvaultConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'eduvault'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        ADMIN_USERNAME = "mohan"
+        ADMIN_PASSWORD = "admin123"
+        ADMIN_EMAIL = "mohanrgbl6629@gmail.com"
+
+        if not User.objects.filter(username=ADMIN_USERNAME).exists():
+            User.objects.create_superuser(
+                username=ADMIN_USERNAME,
+                email=ADMIN_EMAIL,
+                password=ADMIN_PASSWORD
+            )
