167fe7cd582028aac6fa5a923d021c44c72324cd
Mohan Gowda B L <mohanrgbl6629@gmail.com>
2026-03-24 23:52:06 +0530

    cycle working check3

--- eduvault/academics/admin.py
+++ eduvault/academics/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Branche, Semester, Subject,Scheme
+from .models import Branche, Semester, Subject, Scheme
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "get_branch_code", "semester", "scheme")
+
+    def get_branch_code(self, obj):
+        return obj.branch.code if obj.branch else "-"
+
+    get_branch_code.short_description = "Branch"
+
 
 admin.site.register(Branche)
 admin.site.register(Semester)
-admin.site.register(Subject)
-
-
 admin.site.register(Scheme)
--- eduvault/academics/models.py
+++ eduvault/academics/models.py
@@ -29,8 +29,12 @@ class Subject(models.Model):
     branch = models.ForeignKey(Branche, on_delete=models.CASCADE)
     semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
     scheme = models.ForeignKey(Scheme, on_delete=models.CASCADE)
+
     def __str__(self):
-        return self.name
+        branch_code = self.branch.code if self.branch else "N/A"
+        semester_number = self.semester.number if self.semester else "?"
+
+        return f"{self.name} (Sem {semester_number} - {branch_code})"
 # Create your models here.
 from django.db import models
 
